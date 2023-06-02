961741f1850d45ae9b36006bd95f97680380d359
Garrett Strand <gstrand99@gmail.com>
2023-06-01 22:32:00 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,50 @@
+<script lang='ts'>
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-rocket.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase"><a href="/">Garrett Strand</a></strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+        <LightSwitch />
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://linkedin.com/in/garrett-strand-b198a8208/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					LinkedIn
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/gstrand99"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+        <a
+					class="btn btn-sm variant-ghost-surface"
+					href="mailto:contact@garrett-strand.tech"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Email
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,74 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">Welcome to my Portfolio.</h2>
+		<figure>
+			<section class="img-bg" />
+       
+		</figure>
+		<!-- / -->
+		<div class="flex justify-center space-x-2">
+			<a
+				class="btn variant-filled"
+				href="/aboutme"
+				target="_self"
+				rel="noreferrer"
+			>
+				About Me
+			</a>
+      <a
+				class="btn variant-filled"
+				href="/projects"
+				target="_self"
+				rel="noreferrer"
+			>
+				Projects
+			</a>
+		</div>
+		<div class="flex justify-center space-x-2">
+      <a
+				class="btn variant-filled"
+				href="/contact"
+				target="_self"
+				rel="noreferrer"
+			>
+				Contact Me
+			</a>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	figure {
+		@apply flex relative flex-col;
+	}
+	figure svg,
+	.img-bg {
+		@apply w-64 h-64 md:w-80 md:h-80;
+	}
+	.img-bg {
+		@apply absolute z-[-1] rounded-full blur-[50px] transition-all;
+		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
+			glow 5s linear infinite;
+	}
+	@keyframes glow {
+		0% {
+			@apply bg-primary-400/50;
+		}
+		33% {
+			@apply bg-secondary-400/50;
+		}
+		66% {
+			@apply bg-tertiary-400/50;
+		}
+		100% {
+			@apply bg-primary-400/50;
+		}
+	}
+	@keyframes pulse {
+		50% {
+			transform: scale(1.5);
+		}
+	}
+</style>
